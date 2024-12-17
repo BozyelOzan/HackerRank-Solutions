d59f96a03fa01b174ec1fc22ec0ef1664325ef71
Ozan Bozyel <Ozan.Bozyel@hotmail.com>
2024-12-17 15:47:24 +0300

    Create mutations.py

---
+++ Python/Strings/mutations.py
@@ -0,0 +1,11 @@
+#!env\Scripts\python.exe
+
+def mutate_string(string, position, character):
+    string = string[:position] + character + string[position+1:]
+    return f"{string}"
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
