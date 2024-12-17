07a47d2fcd2ca7de3f19eb55e4838543cd9a3565
Ozan Bozyel <Ozan.Bozyel@hotmail.com>
2024-12-17 15:47:22 +0300

    Create string_formating.py

---
+++ Python/Strings/string_formating.py
@@ -0,0 +1,15 @@
+#!env\Scripts\python.exe
+
+def print_formatted(number):
+    w = len(bin(number)[2:])
+    
+    for i in range(1, number+1):
+        deci = str(i)
+        octa = oct(i)[2:]
+        hexa = hex(i)[2:].upper()
+        bina = bin(i)[2:]
+        print(deci.rjust(w),octa.rjust(w),hexa.rjust(w),bina.rjust(w))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
